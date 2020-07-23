179f3b71c3e674bfa529f37afc0faf4b67b25821
Nils Knappmeier <github@knappi.org>
2020-07-24 00:46:45 +0200

    add browserstack snapshots

--- integration-test/browserstack.test.js
+++ integration-test/browserstack.test.js
@@ -23,46 +23,53 @@ initStoryshots({
 	test: imageSnapshot({
 		sizes: ["1280x1024", "1024x768", "800x600", "320x640"],
 		browsers: [
-			// {
-			// 	id: "ie11",
-			// 	capabilities: {
-			// 		os: "windows",
-			// 		os_version: "10",
-			// 		browserName: "IE",
-			// 		browser_version: "11.0",
-			// 		"browserstack.local": true,
-			// 	},
-			// },
-			// {
-			// 	id: "chrome",
-			// 	capabilities: {
-			// 		os: "windows",
-			// 		os_version: "10",
-			// 		browserName: "chrome",
-			// 		"browserstack.local": true,
-			// 	},
-			// },
+			{
+				id: "ie11",
+				capabilities: {
+					os: "windows",
+					os_version: "10",
+					browserName: "IE",
+					browser_version: "11.0",
+					"browserstack.local": true,
+				},
+			},
+			{
+				id: "chrome",
+				capabilities: {
+					os: "windows",
+					os_version: "10",
+					browserName: "chrome",
+					browser_version: "latest",
+					"browserstack.local": true,
+				},
+			},
 			{
 				id: "firefox",
 				capabilities: {
 					os: "windows",
 					os_version: "10",
 					browserName: "firefox",
+					browser_version: "latest",
 					"browserstack.local": true,
 				},
 			},
-			// {
-			// 	id: "safari",
-			// 	capabilities: {
-			// 		os: "os x",
-			// 		browserName: "safari",
-			// 		"browserstack.local": true,
-			// 	},
-			//},
+			{
+				id: "safari",
+				capabilities: {
+					os: "os x",
+					browserName: "safari",
+					browser_version: "latest",
+					"browserstack.local": true,
+				},
+			},
 		],
 		seleniumUrl: browserstackURL,
 		storybookUrl: "http://localhost:9009",
 		snapshotDirectory: __filename + "-snapshots",
+		beforeScreenshot() {
+			// Give the browser a chance to load the emoji font
+			return new Promise((resolve) => setTimeout(resolve, 500));
+		},
 		testTimeoutMillis: 30000,
 	}),
 });
