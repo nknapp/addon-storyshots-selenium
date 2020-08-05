50d8d188d3e6acec1568b1cc5a48cb2061826fd0
Nils Knappmeier <github@knappi.org>
2020-08-05 22:57:40 +0200

    test: add "localIdentifier" to browserstack config
    - see https://www.browserstack.com/local-testing/automate#multiple-local-testing-connections
     for details

--- integration-test/browserstack.test.ts
+++ integration-test/browserstack.test.ts
@@ -1,56 +1,60 @@
 /* eslint-env jest */
 import initStoryshots from "@storybook/addon-storyshots";
-import { doNothing, imageSnapshot, waitMillis } from "../src";
+import { v4 as uuidV4 } from "uuid";
+import { tunnel } from "./test-utils/browserstack-tunnel";
+import { imageSnapshot, waitMillis } from "../src";
 
 import { storybookStaticServer } from "./test-utils/server";
 
 if (process.env.BROWSERSTACK_ACCESS_KEY == null || process.env.BROWSERSTACK_USERNAME == null) {
-	// eslint-disable-next-line no-console
-	console.warn(
-		"if you want to run tests with browserstack, please set the environment variables BROWSERSTACK_USERNAME and BROWSERSTACK_ACCESS_KEY",
-		doNothing()
+	throw new Error(
+		"if you want to run tests with browserstack, please set the environment variables BROWSERSTACK_USERNAME and BROWSERSTACK_ACCESS_KEY"
 	);
 } else {
-	// beforeAll(() => tunnel.start({ verbose: true }), 10000);
-	// afterAll(() => tunnel.stop(), 10000);
-
+	const localIdentifier = uuidV4();
+	beforeAll(() => tunnel.start({ localIdentifier }), 10000);
+	afterAll(() => tunnel.stop(), 10000);
 	const key = process.env.BROWSERSTACK_ACCESS_KEY;
 	const username = process.env.BROWSERSTACK_USERNAME;
 	const browserstackURL = `https://${username}:${key}@hub-cloud.browserstack.com/wd/hub`;
-	const server = storybookStaticServer(9010);
+	const browsers = [
+		{
+			id: "ie11",
+			capabilities: {
+				os: "windows",
+				os_version: "10",
+				browserName: "IE",
+				browser_version: "11.0",
+				resolution: "1920x1080",
+				"browserstack.local": true,
+				"browserstack.localIdentifier": localIdentifier,
+			},
+		},
+		{
+			id: "safari",
+			capabilities: {
+				os: "os x",
+				browserName: "safari",
+				browser_version: "latest",
+				resolution: "1920x1080",
+				"browserstack.local": true,
+				"browserstack.localIdentifier": localIdentifier,
+			},
+		},
+	];
+	const storybookServerPort = 9010;
+	const server = storybookStaticServer(storybookServerPort);
 	beforeAll(async () => server.start());
-	afterAll(async () => server.stop());
 
+	afterAll(async () => server.stop());
 	initStoryshots({
 		framework: "html",
 		suite: "All",
 		test: imageSnapshot({
 			sizes: ["1280x1024", "1024x768", "800x600", "320x640"],
-			browsers: [
-				{
-					id: "ie11",
-					capabilities: {
-						os: "windows",
-						os_version: "10",
-						browserName: "IE",
-						browser_version: "11.0",
-						resolution: "1920x1080",
-						"browserstack.local": true,
-					},
-				},
-				{
-					id: "safari",
-					capabilities: {
-						os: "os x",
-						browserName: "safari",
-						browser_version: "latest",
-						resolution: "1920x1080",
-						"browserstack.local": true,
-					},
-				},
-			],
+			browsers,
 			seleniumUrl: browserstackURL,
-			storybookUrl: "http://localhost:9010",
+			storybookUrl: `http://localhost:${storybookServerPort}`,
 			snapshotBaseDirectory: __filename + "-snapshots",
 			testTimeoutMillis: 30000,
 			beforeFirstScreenshot: waitMillis(3000),
